c568fc29f9c3a31015c34b170facd96792a29952
Aymeric Chauvin <contact@halftheopposite.dev>
2023-05-29 15:29:32 +0200

    Add canvas rendering

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 function start() {
-  console.log("HELLO");
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  if (!canvas) {
+    throw new Error(`Could not find canvas element.`);
+  }
+
+  const context = canvas.getContext("2d");
+  if (!context) {
+    throw new Error(`Could not get context.`);
+  }
+
+  context.fillStyle = "#FF0000";
+  context.fillRect(0, 0, 150, 75);
 }
 
 start();
