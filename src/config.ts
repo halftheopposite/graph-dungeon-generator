38b034db25a5a3bea3ea00c6e81d98709369c7a3
Aymeric Chauvin <contact@halftheopposite.dev>
2023-05-29 18:30:50 +0200

    Add config and types

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,6 @@ export const TILE_WALL = 1;
 
 export const TILE_VOID_COLOR = "rgba(127,0,0)";
 export const TILE_WALL_COLOR = "rgba(0,0,0)";
+
+export const DUNGEON_WIDTH_UNIT = 32;
+export const DUNGEON_HEIGHT_UNIT = 32;
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,42 @@
+//
+// Generic
+//
+export type Position = {
+  x: number;
+  y: number;
+};
+
+export type Dimension = {
+  width: number;
+  height: number;
+};
+
+//
+// Graph
+//
+export type RoomId = string;
+export type RoomType = "start" | "end";
+
+export type GraphRoom = {
+  id: RoomId;
+  type: RoomType;
+  connexions: RoomId[];
+};
+
+export type GraphDungeon = {
+  [roomId: RoomId]: GraphRoom;
+};
+
+//
+// Placed
+//
+export type PlacedRoom = GraphRoom & {
+  position: Position;
+  Dimension: Dimension;
+};
+
+//
+// Tiles
+//
 export type Tile = number;
 export type Tiles = number[][];
