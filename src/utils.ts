15312ab2a910107534940fe008da9768dbff5f81
Aymeric Chauvin <contact@halftheopposite.dev>
2023-06-05 13:07:38 +0200

    Add randomChoice method

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,10 @@
 export function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+/**
+ * Randomly pick a value in an array.
+ */
+export function randomChoice<T>(values: T[]): T {
+  return values[Math.floor(Math.random() * values.length)];
+}
