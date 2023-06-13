6fca24ba24b933b14cd5942f51649322f7bfe731
Aymeric Chauvin <contact@halftheopposite.dev>
2023-06-13 10:05:55 +0200

    Make it easier to generate dungeons and access the repo

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,17 @@ import { logStep } from "./utils";
 /**
  * Entry point to run a dungeon generation and drawing it 🧙‍♂️.
  */
-function start() {
+function generateAndDraw() {
   const rootNode = logStep(`Generate ✅`, () => generate(LARGE));
   logStep(`Draw ✅`, () => draw(rootNode));
 }
 
 /**
- * Will run a high number of dungeon generation and compute a percentage of failed ones.
- * Note: useful to know if you're making progress or regression when updating the dungeon generation code.
+ * ⚠️ UNCOMMENT WHEN NEEDED ⚠️
+ * Will run a high number of dungeon generations and compute a percentage of failed ones.
+ * Useful to know if you're making progress or regressing when updating the dungeon generation algorithm.
  */
-function startGenerationTesting() {
+function testGenerationSuccessRate() {
   let errorCount = 0;
 
   for (let i = 0; i < 10000; i++) {
@@ -30,5 +31,17 @@ function startGenerationTesting() {
   console.log(`${percentage}% of rooms couldn't be placed.`);
 }
 
-start();
-// startGenerationTesting();
+window.onload = () => {
+  generateAndDraw();
+
+  const buttonGenerate = document.getElementById(
+    "button-generate"
+  ) as HTMLButtonElement;
+  buttonGenerate.onclick = () => generateAndDraw();
+
+  const buttonGitHub = document.getElementById(
+    "button-github"
+  ) as HTMLButtonElement;
+  buttonGitHub.onclick = () =>
+    window.open("https://github.com/halftheopposite/graph-dungeon-generator");
+};
